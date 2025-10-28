224b4ccea74b8a2a42e6047a65d63ae6a15ac3ec
Zhu <zhusonglai@Live.cn>
2025-10-28 22:33:31 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/graphqlserver/main.go
@@ -0,0 +1,23 @@
+// cmd/graphqlserver/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "graphqlserver/internal/graphqlserver"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := graphqlserver.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
